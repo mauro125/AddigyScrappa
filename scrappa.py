2ab2f39ef3e86b0f68e86736cbd44d3630323c88
Mauricio Naranjo <narama125@gmail.com>
2022-10-11 01:52:13 -0400

    retrieving reddit timestamp from slack message to only post new message

--- scrappa.py
+++ scrappa.py
@@ -1,9 +1,8 @@
+from slack_token import *
 import json
+from pprint import pprint
 import requests
 import time
-
-from slack_token import *
-
 import logging
 # Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
 from slack_sdk import WebClient
@@ -14,52 +13,61 @@ from slack_sdk.errors import SlackApiError
 # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
 client = WebClient(token=slack_token_code)
 logger = logging.getLogger(__name__)
+timeStampOfRedditMessage = set()
+
+
 # Store conversation history
-conversation_history = []
-# ID of the channel you want to send the message to
-channel_id = "C03PMAFFK50"
+def get_reddit_time_stamp_from_messages_in_slack():
+    # conversation_history = []
+    # ID of the channel you want to send the message to
+    channel_id = "C03PMAFFK50"
 
-try:
-    # Call the conversations.history method using the WebClient
-    # conversations.history returns the first 100 messages by default
-    # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
-    result = client.conversations_history(channel=channel_id)
+    try:
+        limit = 20
+        # Call the conversations.history method using the WebClient
+        # conversations.history returns the first 100 messages by default
+        # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+        result = client.conversations_history(channel=channel_id, limit=limit)
 
-    conversation_history = result["messages"]
-    print(conversation_history)
-    # Print results
-    logger.info("{} messages found in {}".format(len(conversation_history), id))
+        conversation_history = result["messages"]
 
-except SlackApiError as e:
-    logger.error("Error creating conversation: {}".format(e))
+        for message in conversation_history:
+            if message["blocks"][0]["type"] == 'section':
+                timeStampOfRedditMessage.add(message['text'])
+        # print(conversation_history[9]['text'])
+        pprint(conversation_history)
+        logger.info("{} messages found in {}".format(len(conversation_history), id))
+
+    except SlackApiError as e:
+        logger.error("Error creating conversation: {}".format(e))
+
 
 slack_channel = '#general'
-comments = set()
 
 
 # def post_message_to_slack(text, blocks):
-def post_message_to_slack(query):
-    postDateCreation = ''
-    blocks = []
-    urlToGetPosts = f"https://api.pushshift.io/reddit/submission/search/?q={query}&size=1"
+def new_post_to_slack(query):
+    get_reddit_time_stamp_from_messages_in_slack()
+    # postDateCreation = ''
+    # blocks = []
+    urlToGetPosts = f"https://api.pushshift.io/reddit/submission/search/?q={query}&size=3"
     r = requests.get(urlToGetPosts)
     data = json.loads(r.text, strict=False)
 
-    # Reversing the data returned so the latest message will post last
+    # Reversing the data returned so the newest message will post last
     count = len(data['data']) - 1
     for item in reversed(data['data']):
         # for item in reversed(data['data'][count]):
-        if data['data'][count]['permalink'] not in comments:
+        # if data['data'][count]['permalink'] not in comments:
+        if str(data['data'][count]['created_utc']) not in timeStampOfRedditMessage:
             print('not in set adding new comment')
             print(data['data'][count]['permalink'])
-            comments.add(item['permalink'])
             print(count, "__", item['permalink'])
 
-            post_created_date = time.strftime('%b %d %Y %H:%M%p',
+            post_created_date = time.strftime('%b %d %Y %I:%M%p',
                                               time.localtime(data['data'][count]['created_utc']))
-            print(post_created_date)
             post_title = data['data'][count]['title']
-            url_of_post = data['data'][count]['url']
+            url_of_post = data['data'][count]['full_link']
             user_name = data['data'][count]['author']
             if data['data'][count]['subreddit_id'] is not None:
                 sub_reddit = f"r/{data['data'][count]['subreddit']}"
@@ -71,7 +79,8 @@ def post_message_to_slack(query):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": f"A new Reddit mention:\n*<{url_of_post}|{post_title}>*"
+                        "text": "A new Reddit post mentions the keyword: *" + query.replace('\"', '').capitalize() +
+                                f"*\n*<{url_of_post}|{post_title}>*"
                     }
                 },
                 {
@@ -102,7 +111,6 @@ def post_message_to_slack(query):
                             "type": "button",
                             "text": {
                                 "type": "plain_text",
-                                # "emoji": true,
                                 "text": "Acknowledged"
                             },
                             "style": "primary",
@@ -112,7 +120,6 @@ def post_message_to_slack(query):
                             "type": "button",
                             "text": {
                                 "type": "plain_text",
-                                # "emoji": true,
                                 "text": "Deny"
                             },
                             "style": "danger",
@@ -121,28 +128,22 @@ def post_message_to_slack(query):
                     ]
                 }
             ]
+            # sending reddit post time stamp to slack, so we can later compare if the post has already been posted
             postDateCreation = data['data'][count]['created_utc']
-            # post_message_to_slack(text)
+
+            requests.post('https://slack.com/api/chat.postMessage', {
+                'token': slack_token_code,
+                'channel': slack_channel,
+                'text': postDateCreation,
+                'blocks': json.dumps(blocks) if blocks else None
+            }).json()
         else:
             print('no new posts')
         # count += 1
         count -= 1
 
-    return requests.post('https://slack.com/api/chat.postMessage', {
-        'token': slack_token_code,
-        'channel': slack_channel,
-        'text': postDateCreation,
-        'blocks': json.dumps(blocks) if blocks else None
-    }).json()
+    # return response
 
-
-def get_messages_from_slack():
-    return requests.get('https://slack.com/api/conversations.history', {
-        'token': 'xoxp-3788761613207-3803299958707-3816032296929-7ec486d965edb3afc2311f02c4302ca9',
-        'channel': 'C03PMAFFK50',
-        'limit': 1000,
-        # 'Authorization': slack_token_code
-    }).json()
 
 # messages = get_messages_from_slack()
 # print(messages)
@@ -167,97 +168,14 @@ while userInput != '5':
     print("\n\nenter 1 to check for new posts and comments")
     userInput = input('enter choice: ')
     if userInput == '1':
-        # count = 0
-        query = "addigy"
+        queries = ['addigy', 'mosyle', 'kandji', 'Jamf', "\"Manage Apple Devices\""]
+        # queries = ['kandji']
         # url = f"https://api.pushshift.io/reddit/search/comment/?q={query}"
         # url = f"https://api.pushshift.io/reddit/search/submission/?q={query}"
         # url = f"https://api.pushshift.io/reddit/submission/search/?q={query}&sort=asc"
         # url = f"https://api.pushshift.io/reddit/submission/search/?q={query}&size=2"
         # r = requests.get(url)
-        #
-        # data = json.loads(r.text, strict=False)
-        # # data = data['data'].reverse()
-        # # reverseData = reversed(data['data'])
-        #
-        # count = len(data['data']) - 1
-        # for item in reversed(data['data']):
-        #     # for item in reversed(data['data'][count]):
-        #     if data['data'][count]['permalink'] not in comments:
-        #         print('not in set adding new comment')
-        #         print(data['data'][count]['permalink'])
-        #         comments.add(item['permalink'])
-        #         print(count, "__", item['permalink'])
-        #
-        #         post_created_date = time.strftime('%b %d %Y %H:%M%p',
-        #                                           time.localtime(data['data'][count]['created_utc']))
-        #         print(post_created_date)
-        #         post_title = data['data'][count]['title']
-        #         url_of_post = data['data'][count]['url']
-        #         user_name = data['data'][count]['author']
-        #         if data['data'][count]['subreddit_id'] is not None:
-        #             sub_reddit = f"r/{data['data'][count]['subreddit']}"
-        #         else:
-        #             sub_reddit = 'no info available'
-        #
-        #         blocks = [
-        #             {
-        #                 "type": "section",
-        #                 "text": {
-        #                     "type": "mrkdwn",
-        #                     "text": f"A new Reddit mention:\n*<{url_of_post}|{post_title}>*"
-        #                 }
-        #             },
-        #             {
-        #                 "type": "section",
-        #                 "fields": [
-        #                     {
-        #                         "type": "mrkdwn",
-        #                         "text": f"*User Name:*\nu/{user_name}"
-        #                     },
-        #                     {
-        #                         "type": "mrkdwn",
-        #                         "text": f"*Sub-reddit:*\n{sub_reddit}"
-        #                     },
-        #                     {
-        #                         "type": "mrkdwn",
-        #                         "text": "*Type:*\nPost\n"
-        #                     },
-        #                     {
-        #                         "type": "mrkdwn",
-        #                         "text": f"*When:*\n{post_created_date}\n"
-        #                     },
-        #                 ]
-        #             },
-        #             {
-        #                 "type": "actions",
-        #                 "elements": [
-        #                     {
-        #                         "type": "button",
-        #                         "text": {
-        #                             "type": "plain_text",
-        #                             # "emoji": true,
-        #                             "text": "Acknowledged"
-        #                         },
-        #                         "style": "primary",
-        #                         "value": "click_me_123"
-        #                     },
-        #                     {
-        #                         "type": "button",
-        #                         "text": {
-        #                             "type": "plain_text",
-        #                             # "emoji": true,
-        #                             "text": "Deny"
-        #                         },
-        #                         "style": "danger",
-        #                         "value": "click_me_123"
-        #                     }
-        #                 ]
-        #             }
-        #         ]
-        #         text = data['data'][count]['created_utc']
-    post_message_to_slack(query)
-    # else:
-    # print('no new posts')
-    # count += 1
-    # count -= 1
-    print(comments)
+
+    for query in queries:
+        new_post_to_slack(query)
+    get_reddit_time_stamp_from_messages_in_slack()
